e8b26c6af112ddd67108bf0111ed7cbdc36a4423
chimangokalua <159056929+chimangokalua@users.noreply.github.com>
2024-05-30 20:37:39 +0300

    Main.java

--- Main.java
+++ Main.java
@@ -1,15 +1,34 @@
-public class Main{
-    
-	public static void main(String[] args) {
-	    
-    String[] copyFrom = {
-            "Affogato", "Americano", "Cappucciono", "Cortado", "Espresso", "Frapuchino", "Freddo", "Macchiato", "Lungo", "Marocchino", "Ristretto"
-        };
+public class Task2ArrayCode {
+    public static void main(String[] args) {
+        // Declare and initialize the array
+        int[] ten = {10, 20, 20, 40, 50, 60, 70, 80, 90, 100};
 
-        String[] copyTo = new String[7];
-        System.arraycopy(copyFrom, 2, copyTo, 0, 7);
-        for (String coffee : copyTo) {
-            System.out.print(coffee + " ");
+        // Compute the sum of the elements
+        int sum = 0;
+        for (int num : ten) {
+            sum += num;
+        }
+        System.out.println("Sum: " + sum);
+
+        // Sort the array in descending order
+        java.util.Arrays.sort(ten);
+        for (int i = 0; i < ten.length / 2; i++) {
+            int temp = ten[i];
+            ten[i] = ten[ten.length - 1 - i];
+            ten[ten.length - 1 - i] = temp;
+        }
+
+        // Replace 100 with 1000
+        for (int i = 0; i < ten.length; i++) {
+            if (ten[i] == 100) {
+                ten[i] = 1000;
+            }
+        }
+
+        // Print the sorted array
+        System.out.println("Sorted array in descending order:");
+        for (int num : ten) {
+            System.out.println(num);
         }
     }
 }
