7c2c1ab4e7b52facdaeed09201b02338981d9c70
chimangokalua <159056929+chimangokalua@users.noreply.github.com>
2024-05-30 20:38:03 +0300

    arraySorting.java

--- arraySorting.java
+++ arraySorting.java
@@ -1,15 +1,1 @@
-public class arraySorting {
-    
-    public static void main(String[] args) {
-        
-        int[] myArray= {
-            5,2,8,3,1,6,4
-            };
-            
-		Arrays.sort(myArray);
-		System.out.System.out.println("Sorted Array:");
-		for (int i: myArray) {
-		    System.out.print(i + "");
-		}
-	}
-}
+
