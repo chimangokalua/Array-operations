b25058c36a2af602da72935b26d84a9d3e3558a4
chimangokalua <159056929+chimangokalua@users.noreply.github.com>
2024-05-30 20:40:23 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,16 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        // Declare and initialize the array
+    
         int[] ten = {10, 20, 20, 40, 50, 60, 70, 80, 90, 100};
 
-        // Compute the sum of the elements
+        
         int sum = 0;
         for (int num : ten) {
             sum += num;
         }
         System.out.println("Sum: " + sum);
 
-        // Sort the array in descending order
+        
         java.util.Arrays.sort(ten);
         for (int i = 0; i < ten.length / 2; i++) {
             int temp = ten[i];
@@ -18,14 +18,14 @@ public class Main {
             ten[ten.length - 1 - i] = temp;
         }
 
-        // Replace 100 with 1000
+        
         for (int i = 0; i < ten.length; i++) {
             if (ten[i] == 100) {
                 ten[i] = 1000;
             }
         }
 
-        // Print the sorted array
+    
         System.out.println("Sorted array in descending order:");
         for (int num : ten) {
             System.out.println(num);
